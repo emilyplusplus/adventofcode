5c4e61f0a36b8291671ff47ad691165af179aba9
Emily Carlsen <emily.carlsen@shopify.com>
2021-12-07 10:04:34 -0500

    Day 7

--- 2021/day7/index.js
+++ 2021/day7/index.js
@@ -1,5 +1,31 @@
+function calc_median(arr) {
+  const mid = Math.floor(arr.length / 2),
+    nums = [...arr].sort((a, b) => a - b);
+  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
+
 function day7(input) {
-  return 0
+  const crabs_x = input.split(',').map(x => parseInt(x))
+  const median = calc_median(crabs_x)
+  const mean = Math.round(crabs_x.reduce((a, b) => a + b) / crabs_x.length); //or floor or ceil, depends
+
+  //console.log(crabs_x, median, mean)
+
+  let fuelRequired = 0
+
+  crabs_x.forEach(x => {
+    fuelRequired += Math.abs(x - median)
+  })
+
+  let fuelRequired2 = 0
+
+  crabs_x.forEach(x => {
+    const steps = Math.abs(x - mean) + 1
+    const fuel = (steps*(steps-1))/2
+    fuelRequired2 += fuel
+  })
+
+  return [fuelRequired, fuelRequired2]
 }
 
 module.exports = day7
--- 2021/day7/index.test.js
+++ 2021/day7/index.test.js
@@ -3,5 +3,5 @@ const readFile = require('../utils/file')
 const day7 = require('./index');
 
 test('example input', () => {
-    expect(day7(readFile('./day7/test.txt'))).toBe(0);
+    expect(day7(readFile('./day7/test.txt', true))).toEqual([37,168]);
 });
