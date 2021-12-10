fe595d40bca155a9fd45dbe0ce9fd6d611cbc4b0
Emily Carlsen <emilytcarlsen@gmail.com>
2021-12-10 12:51:45 -0500

    Part 1 partial

--- 2021/day10/index.js
+++ 2021/day10/index.js
@@ -1,6 +1,25 @@
 function day10(input) {
   // Part 1
 
+  input.forEach(line => {
+    const brackets = line.split('')
+    let stack = new Array();
+
+    
+
+    brackets.forEach(bracket => {
+      if(['(','[','{','<'].includes(bracket)) {
+        stack.push(bracket)
+      } else if([')',']','}','>'].includes(bracket)) {
+        let popped = stack.pop()
+        switch(bracket) {
+          case ')':
+            if(popped != '(') 
+        }
+      }
+    })
+  })
+
   // Part 2
 
   return [0, 0]
